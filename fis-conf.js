baab0e26447b945232657eb013730162d3247cf9
xing-zhi <xing_zhi@126.com>
2017-03-17 15:46:41 +0800

    use fis-parser-node-sass to translate scss to css

--- fis-conf.js
+++ fis-conf.js
@@ -2,3 +2,11 @@ fis.match('js/*.js', {
   parser: fis.plugin('babel-6.x'),
   rExt: 'js'
 });
+
+fis.match('scss/(*.scss)', {
+  isCssLike: true,
+  parser: fis.plugin('node-sass'),
+  rExt: '.css',
+  // change css folder from scss to css
+  release: '/css/$1'
+});
